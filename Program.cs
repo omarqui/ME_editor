57c284a0e36844ee844f0b621627d40fb7f10756
Omarqui <omarqui199@gmail.com>
2021-09-27 20:51:52 -0400

    Create project
    This is the initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ME_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
